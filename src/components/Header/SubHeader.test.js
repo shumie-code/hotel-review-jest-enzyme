c6994dfbe0b2f1c85472d9e917cf1ca0b495572e
Petros Shumie <shumie.peter@gmail.com>
2020-07-16 16:06:37 -0400

    Removed all test scenarios that use snapshots and replaced with more concrete tests that are less vulnerable once the code changes. These tests will contiue to keep working if we change any props that make refactoring eaiser.

--- src/components/Header/SubHeader.test.js
+++ src/components/Header/SubHeader.test.js
@@ -1,36 +1,38 @@
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
-import SubHeader from './SubHeader';
+import { shallow } from 'enzyme';
+import SubHeader, { Title, SubHeaderButton } from './SubHeader';
 
 describe('the <SubHeader /> component', () => {
-    const renderer = new ShallowRenderer();
 
     it('should render', () => {
-        renderer.render(<SubHeader />);
-        const component = renderer.getRenderOutput();
-
+        const component = shallow(<SubHeader />);
+        
         expect(component).toMatchSnapshot();
     });
 
     it('should render with a dynamic title', () => {
-        renderer.render(<SubHeader title='Test Application Test' />);
-        const component = renderer.getRenderOutput();
-
-        expect(component).toMatchSnapshot();
+    const title = 'Test Application';  
+    const component = shallow(<SubHeader title={title} />);
+    
+    expect(component.find(Title).text()).toEqual(title);
     });
 
-    it('should render with a goBack button', () => {
-        renderer.render(<SubHeader goBack={() => {}} />);
-        const component = renderer.getRenderOutput();
+    it('should render with a button and handle the  onClick event', () => {
+        const mockGoBack = jest.fn();
+        const mockOpenForm = jest.fn();
+        const component = shallow(<SubHeader goBack={mockGoBack} openForm={mockOpenForm} />);
+        
+        const goBackButton = component.find(SubHeaderButton).at(0);
+        expect(goBackButton.exists()).toBe(true);
 
-        expect(component).toMatchSnapshot();
-    });
+        const openFormButton = component.find(SubHeaderButton).at(1);
+        expect(openFormButton.exists()).toBe(true);
 
-    it('should render with a form button', () => {
-        renderer.render(<SubHeader openForm={() => {}} />);
-        const component = renderer.getRenderOutput();
+        goBackButton.simulate('click');
+        expect(mockGoBack).toHaveBeenCalled();
 
-        expect(component).toMatchSnapshot();
+        openFormButton.simulate('click');
+        expect(mockOpenForm).toHaveBeenCalled();
     });
 });
 
